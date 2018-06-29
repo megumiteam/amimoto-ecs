8aac03bd366b505271dcb866cbc119c7d1915ba6
Daniel Olson <emaildano@gmail.com>
2018-06-29 18:21:49 -0400

    updated module names

--- ecs.tf
+++ ecs.tf
@@ -6,8 +6,8 @@ module "ecs" {
   source = "modules/ecs"
 
   environment          = "${var.environment}"
-  cluster              = "${var.environment}"
-  cloudwatch_prefix    = "${var.environment}"           #See ecs_instances module when to set this and when not!
+  cluster              = "${var.cluster}"
+  cloudwatch_prefix    = "${var.cluster}-${var.environment}" #See ecs_instances module when to set this and when not!
   vpc_cidr             = "${var.vpc_cidr}"
   public_subnet_cidrs  = "${var.public_subnet_cidrs}"
   private_subnet_cidrs = "${var.private_subnet_cidrs}"
@@ -21,12 +21,13 @@ module "ecs" {
 }
 
 resource "aws_key_pair" "ecs" {
-  key_name   = "ecs-key-${var.environment}"
+  key_name   = "ecs-key-${var.cluster}-${var.environment}"
   public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCtMljjj0Ccxux5Mssqraa/iHHxheW+m0Rh17fbd8t365y9EwBn00DN/0PjdU2CK6bjxwy8BNGXWoUXiSDDtGqRupH6e9J012yE5kxhpXnnkIcLGjkAiflDBVV4sXS4b3a2LSXL5Dyb93N2GdnJ03FJM4qDJ8lfDQxb38eYHytZkmxW14xLoyW5Hbyr3SXhdHC2/ecdp5nLNRwRWiW6g9OA6jTQ3LgeOZoM6dK4ltJUQOakKjiHsE+jvmO0hJYQN7+5gYOw0HHsM+zmATvSipAWzoWBWcmBxAbcdW0R0KvCwjylCyRVbRMRbSZ/c4idZbFLZXRb7ZJkqNJuy99+ld41 ecs@aws.fake"
 }
 
 variable "vpc_cidr" {}
 variable "environment" {}
+variable "cluster" {}
 variable "max_size" {}
 variable "min_size" {}
 variable "desired_capacity" {}
--- ecs.tfvars
+++ ecs.tfvars
@@ -1,6 +1,8 @@
 vpc_cidr = "10.0.0.0/16"
 
-environment = "acc"
+environment = "pre-alpha"
+
+cluster = "amimoto"
 
 public_subnet_cidrs = ["10.0.0.0/24", "10.0.1.0/24"]
 
